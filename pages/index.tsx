327955dc7b867e010ee968c37c7140050e410a00
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-16 00:45:41 +0700

    fix: conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,48 @@
+"use client";
+
 import { NextPage } from "next";
 import Head from "next/head";
 import Layout from "@/components/layout";
 import Image from "next/image";
+import { useContext } from "react";
+import { AuthContext } from "@/store/context/AuthContext";
+import { useState, useEffect } from "react";
+import socket from "@/config/socket/socket";
+import { IRoom } from "@/types/auth-store";
 
 const Index: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const { state } = useContext(AuthContext);
+
+  useEffect(() => {
+    socket.on("room", (room: IRoom) => {
+      setRooms([...rooms, room]);
+    });
+  }, [rooms]);
+
+  useEffect(() => {
+    socket.emit("get-room-data");
+    socket.on("room-data", (rooms: IRoom[]) => {
+      setRooms(rooms);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <Layout
       withTopBar
       topBarElement={
         <div className="flex items-center w-full px-6">
           <div className="flex items-center gap-5">
-            <div className="w-12 h-12 rounded-full bg-brand-blue-500"></div>
-            <p className="text-brand-xl leading-brand-xl">Sigit Wahyudi</p>
+            <Image
+              src={`https://ui-avatars.com/api/?name=${state.user?.name}&background=random`}
+              alt={`showing ${state.user?.name} avatar image`}
+              width={48}
+              height={48}
+              className="w-12 h-12 rounded-full object-cover"
+            />
+            <p className="text-brand-xl leading-brand-xl">{state.user?.name}</p>
           </div>
         </div>
       }
@@ -35,28 +66,37 @@ const Index: NextPage = () => {
           />
         </div>
         <div className="space-y-5">
-          {Array.from({
-            length: 5,
-          }).map((_, index) => (
-            <div
-              key={index}
-              className="bg-brand-gray-400 rounded-[10px] h-fit flex items-center relative"
-            >
-              <Image
-                src="/assets/icons/room.png"
-                alt="room illustration"
-                width={200}
-                height={200}
-              />
-              <div className="flex flex-col gap-5">
-                <h1 className="text-brand-xl leading-brand-xl">Room Name</h1>
-                <p className="text-base leading-4">Code : 252525</p>
-                <button className="bg-brand-blue-500 rounded-full text-base leading-4 py-[6px] max-w-[80px] text-white">
-                  Join
-                </button>
+          {isLoading ? (
+            <div>Loading</div>
+          ) : (
+            rooms?.map((room, index) => (
+              <div
+                key={index}
+                className="bg-brand-gray-400 rounded-[10px] h-fit flex items-center relative"
+              >
+                <Image
+                  src="/assets/icons/room.png"
+                  alt="room illustration"
+                  width={200}
+                  height={200}
+                />
+                <div className="flex flex-col gap-5">
+                  <h1 className="text-brand-xl leading-brand-xl">
+                    {room.name}
+                  </h1>
+                  <p className="text-base leading-4">
+                    Code <br /> {room._id}
+                  </p>
+                  <button
+                    className="bg-brand-blue-500 rounded-full text-base leading-4 py-[6px] max-w-[80px] text-white"
+                    onClick={() => {}}
+                  >
+                    Join
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </Layout>
