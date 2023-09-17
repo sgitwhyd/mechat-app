19dc94261c713a2adfdc72f332e399535d8efd69
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-17 10:43:36 +0700

    style: fix sticky search bar & change room image

--- components/room-card.tsx
+++ components/room-card.tsx
@@ -12,10 +12,11 @@ const RoomCard = ({ room, handleJoin }: IRoomCardProps) => {
   return (
     <div className="bg-brand-gray-400 rounded-[10px] h-fit flex items-center relative">
       <Image
-        src="/assets/icons/room.png"
+        src="/assets/icons/room.svg"
         alt="room illustration"
-        width={200}
-        height={200}
+        width={250}
+        height={250}
+        priority
       />
       <div className="flex flex-col gap-5">
         <h1 className="text-brand-xl leading-brand-xl">{name}</h1>
@@ -23,7 +24,7 @@ const RoomCard = ({ room, handleJoin }: IRoomCardProps) => {
           Code <br /> {code}
         </p>
         <button
-          className="bg-brand-blue-500 rounded-full text-base leading-4 py-[6px] max-w-[80px] text-white"
+          className="bg-brand-blue-500 rounded-full text-base leading-4 py-2 px-[22px] max-w-[80px] text-white"
           onClick={handleJoin}
         >
           Join
