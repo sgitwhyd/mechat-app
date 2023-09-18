2a070ae0571d04a83fbbbb4d0d51901212c6d375
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-18 18:38:51 +0700

    refactor: implement chat functionality

--- pages/chats/[room_id].tsx
+++ pages/chats/[room_id].tsx
@@ -1,20 +1,64 @@
 "use client";
 
-import React, { useState } from "react";
-import Layout from "@/components/layout";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-type chat = {
-  user_id: number;
-  message: string;
-};
+import Layout from "@/components/layout";
+import { AuthContext } from "@/store/context/AuthContext";
+import socket from "@/config/socket/socket";
+import { messageProps, IRoom } from "@/types/chat";
+import { getRoomDetail } from "@/services/room.service";
 
 const Chats = () => {
   const router = useRouter();
-  const user_id = 1;
-  const [chats, setChats] = useState<chat[]>([]);
+  const { state } = useContext(AuthContext);
+  const user_id = state.user?.user_id;
+  const room_id = router.query.room_id;
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const [room, setRoom] = useState<IRoom>();
+  const [chats, setChats] = useState<messageProps[]>([]);
   const [chat, setChat] = useState<string>("");
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  }, [chats.length]);
+
+  useEffect(() => {
+    (async () => {
+      if (room_id) {
+        const { data } = await getRoomDetail(room_id as string);
+        const room = data.data;
+
+        setRoom(room);
+      }
+    })();
+
+    if (room_id && state.user?.user_id) {
+      socket.emit("leave-room", user_id);
+      socket.emit("join", {
+        user_id: state.user?.user_id,
+        room_id: room_id,
+      });
+    }
+  }, [room_id, state.user?.user_id]);
+
+  useEffect(() => {
+    socket.on("chat", (chat: messageProps) => {
+      setChats([...chats, chat]);
+    });
+  }, [chats]);
+
+  useEffect(() => {
+    socket.emit("get-chats-history", { room_id: room_id });
+    socket.on("get-chats", (chats: messageProps[]) => {
+      setChats(chats);
+    });
+  }, [room_id]);
 
   const onChatInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -22,22 +66,25 @@ const Chats = () => {
   };
 
   const handleSendMessage = () => {
-    const newChat = {
-      user_id: 1,
-      message: chat,
-    };
-
-    setChats([...chats, newChat]);
+    socket.emit("store-chat", {
+      room_id: room_id as string,
+      text: chat,
+    });
     setChat("");
+  };
+
+  const handleBack = () => {
+    socket.emit("leave-room");
+    router.back();
   };
 
   return (
     <Layout
       withTopBar
-      title="Room Name"
+      title={room?.name}
       customClass="fixed top-0 bg-white z-10"
       icon={
-        <button onClick={() => router.back()}>
+        <button onClick={handleBack}>
           <Image
             src="/assets/icons/arrow-left.svg"
             alt="arrow left icon"
@@ -48,32 +95,31 @@ const Chats = () => {
       }
     >
       <div className="flex-1 w-full relative px-[27px]">
-        <div className=" pb-28 space-y-5 mt-20 overflow-y-auto">
+        <div className=" pb-28 space-y-5 mt-20 overflow-y-auto" ref={bottomRef}>
           {chats.map((chat, index) => (
             <div key={index} className="">
               <p
                 className={`${
-                  chat.user_id === user_id ? "text-end" : ""
+                  chat.user_id._id === user_id ? "text-end" : ""
                 } mb-2 text-xs font-semibold`}
               >
-                Sigit
+                {chat.user_id.name}
               </p>
               <div
                 className={`rounded-[10px] min-h-[45px] py-4 px-5 w-fit text-[14px] ${
-                  chat.user_id === user_id
+                  chat.user_id._id === user_id
                     ? "bg-brand-blue-500 ml-auto text-white"
                     : "bg-brand-gray-400"
                 }`}
               >
-                {chat.message}
+                {chat.text}
               </div>
               <div
                 className={`${
-                  chat.user_id === user_id ? "text-end" : ""
+                  chat.user_id._id === user_id ? "text-end" : ""
                 } mb-2 text-[9px] font-semibold mt-2`}
               >
-                {/* {new Date().toISOString()} */}
-                testing
+                {chat.createdAt}
               </div>
             </div>
           ))}
