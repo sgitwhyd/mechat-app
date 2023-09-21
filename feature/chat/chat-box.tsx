33265c9166d32cbb55ebbfdb7004e536605b1528
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-21 11:18:53 +0700

    feat: create chatbox component

---
+++ feature/chat/chat-box.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { messageProps } from "@/types/chat";
+
+type ChatBoxProps = {
+  chat: messageProps;
+  isSender: boolean;
+};
+
+const ChatBox = ({ isSender, chat }: ChatBoxProps) => {
+  return (
+    <div>
+      <p className={`${isSender ? "text-end" : ""} mb-2 text-xs font-semibold`}>
+        {chat.user_id.name}
+      </p>
+      <div
+        className={`rounded-[10px] min-h-[45px] py-4 px-5 w-fit text-[14px] ${
+          isSender
+            ? "bg-brand-blue-500 ml-auto text-white"
+            : "bg-brand-gray-400"
+        }`}
+      >
+        {chat.text}
+      </div>
+      <div
+        className={`${
+          isSender ? "text-end" : ""
+        } mb-2 text-[9px] font-semibold mt-2`}
+      >
+        {chat.createdAt}
+      </div>
+    </div>
+  );
+};
+
+export default ChatBox;
