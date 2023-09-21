a6f80d54bc5ba48c00a9fd71293cebf8e172e761
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-21 11:19:55 +0700

    refactor: spliting code

--- pages/chats/[room_id].tsx
+++ pages/chats/[room_id].tsx
@@ -9,6 +9,8 @@ import { AuthContext } from "@/store/context/AuthContext";
 import socket from "@/config/socket/socket";
 import { messageProps, IRoom } from "@/types/chat";
 import { getRoomDetail } from "@/services/room.service";
+import ChatBox from "@/feature/chat/chat-box";
+import SendMessage from "@/feature/chat/send-message";
 
 const Chats = () => {
   const router = useRouter();
@@ -19,7 +21,6 @@ const Chats = () => {
 
   const [room, setRoom] = useState<IRoom>();
   const [chats, setChats] = useState<messageProps[]>([]);
-  const [chat, setChat] = useState<string>("");
 
   useEffect(() => {
     bottomRef.current?.scrollIntoView({
@@ -60,19 +61,6 @@ const Chats = () => {
     });
   }, [room_id]);
 
-  const onChatInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setChat(e.target.value);
-  };
-
-  const handleSendMessage = () => {
-    socket.emit("store-chat", {
-      room_id: room_id as string,
-      text: chat,
-    });
-    setChat("");
-  };
-
   const handleBack = () => {
     socket.emit("leave-room");
     router.back();
@@ -96,67 +84,16 @@ const Chats = () => {
     >
       <div className="flex-1 w-full relative px-[27px]">
         <div className=" pb-28 space-y-5 mt-20 overflow-y-auto" ref={bottomRef}>
-          {chats.map((chat, index) => (
-            <div key={index} className="">
-              <p
-                className={`${
-                  chat.user_id._id === user_id ? "text-end" : ""
-                } mb-2 text-xs font-semibold`}
-              >
-                {chat.user_id.name}
-              </p>
-              <div
-                className={`rounded-[10px] min-h-[45px] py-4 px-5 w-fit text-[14px] ${
-                  chat.user_id._id === user_id
-                    ? "bg-brand-blue-500 ml-auto text-white"
-                    : "bg-brand-gray-400"
-                }`}
-              >
-                {chat.text}
-              </div>
-              <div
-                className={`${
-                  chat.user_id._id === user_id ? "text-end" : ""
-                } mb-2 text-[9px] font-semibold mt-2`}
-              >
-                {chat.createdAt}
-              </div>
-            </div>
+          {chats.map((chat) => (
+            <ChatBox
+              key={chat._id}
+              chat={chat}
+              isSender={chat.user_id._id === user_id}
+            />
           ))}
         </div>
         <div className="flex w-full">
-          <div className="flex justify-between py-5 pr-5 gap-3 bottom-0 bg-white fixed w-full max-w-lg">
-            <div className="bg-brand-gray-400 rounded-[10px] py-[18px] pl-[18px] flex gap-5 w-full ">
-              <div className="">
-                <Image
-                  src="/assets/icons/emot.svg"
-                  alt="emot icon"
-                  width={24}
-                  height={24}
-                />
-              </div>
-              <input
-                type="text"
-                value={chat}
-                placeholder="Write Here.."
-                className="bg-transparent w-full focus:outline-none"
-                onChange={onChatInputChange}
-              />
-            </div>
-            <div
-              className="w-[100px] cursor-pointer"
-              onClick={handleSendMessage}
-            >
-              <div className="w-[60px] h-[60px] bg-brand-blue-500 rounded-full flex items-center justify-center">
-                <Image
-                  src="/assets/icons/send-icons.svg"
-                  alt="send icon"
-                  width={24}
-                  height={24}
-                />
-              </div>
-            </div>
-          </div>
+          <SendMessage room_id={room_id as string} />
         </div>
       </div>
     </Layout>
